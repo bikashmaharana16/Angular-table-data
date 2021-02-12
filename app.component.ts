f06277b79bcfd258fc68a784368d2e74992e7654
Bikash Kumar Maharana <76558318+bikashmaharana16@users.noreply.github.com>
2021-02-13 00:02:24 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+    students=[
+        {
+            "First_name": "Travis",
+            "last_name": "Bickle",
+            "address": {
+                "address1": "vv road",
+                "city": "bhubaneswar",
+                "state":"Odisha"
+            },
+            
+        },
+        {
+            "First_name": "Travis1",
+            "last_name": "Bickle1",
+            "address": {
+                "address1": "vv road1",
+                "city": "bhubaneswar1",
+                "state":"Odisha1"
+        }
+    },
+    {
+        "First_name": "Travis2",
+        "last_name": "Bickle2",
+        "address": {
+            "address1": "vv road2",
+            "city": "bhubaneswar2",
+            "state":"Odisha2"
+    }
+},
+{
+    "First_name": "Travis3",
+    "last_name": "Bickle3",
+    "address": {
+        "address1": "vv road3",
+        "city": "bhubaneswar3",
+        "state":"Odisha3"
+}
+},
+{
+    "First_name": "Travis3",
+    "last_name": "Bickle3",
+    "address": {
+        "address1": "vv road3",
+        "city": "bhubaneswar3",
+        "state":"Odisha3"
+}
+},
+{
+    "First_name": "Travis3",
+    "last_name": "Bickle2",
+    "address": {
+        "address1": "vv road3",
+        "city": "bhubaneswar3",
+        "state":"Odisha3"
+}
+},
+    
+          ];
+          config = `As you wish`
+        }
+       
+
